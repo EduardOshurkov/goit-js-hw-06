01f35cc894dcfd2bba55640fe752a0123c945fd2
EduardOshurkov <drd2993@gmail.com>
2022-05-20 08:38:13 +0300

    поправил цикл
    Исправил обращение к ДОМу в цикле.

--- js/task-01.js
+++ js/task-01.js
@@ -4,9 +4,9 @@ const categoriesEl = document.querySelector('#categories');
 const listCategories = document.querySelectorAll('.item');
 
 
+
 const numberOfCategories = categoriesEl.children;
 console.log('Number of categories: ' + numberOfCategories.length);
-
 
 for (const element of listCategories) {
     console.log(`Category: ` + element.firstElementChild.textContent);
